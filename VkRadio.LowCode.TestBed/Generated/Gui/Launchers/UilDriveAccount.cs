8683f86204eca7b182c75aeb7e3109243accb209
vkradio <37143087+vkradio@users.noreply.github.com>
2021-04-22 14:28:11 +0500

    LOWC-2 Implemented simple testbed imitating generated CRUDs

---
+++ VkRadio.LowCode.TestBed/Generated/Gui/Launchers/UilDriveAccount.cs
@@ -0,0 +1,37 @@
+﻿using VkRadio.LowCode.Gui.WinForms;
+using VkRadio.LowCode.Orm;
+using VkRadio.LowCode.TestBed.Generated.Gui.Elements;
+using VkRadio.LowCode.TestBed.Generated.Gui.Lists;
+using VkRadio.LowCode.TestBed.Generated.Model.Storage;
+
+namespace VkRadio.LowCode.TestBed.Generated.Gui.Launchers
+{
+    /// <summary>
+    /// UI Means Launcher for objects of type Drive Account
+    /// </summary>
+    public class UilDriveAccount: UILauncher
+    {
+        /// <summary>
+        /// Lancher&apos; constructor
+        /// </summary>
+        public UilDriveAccount()
+        {
+            _storage = StorageRegistry.Instance.DriveAccountStorage;
+            _dotName = "Drive Account";
+        }
+
+        /// <summary>
+        /// Creating of the object&apos;s Card
+        /// </summary>
+        protected override DOCard CreateDOCard(DbMappedDOT in_o)
+        {
+            var panel = new DOPDriveAccount();
+            var card = new DOCard(_storage, in_o, _dotName, panel);
+            return card;
+        }
+        /// <summary>
+        /// Creating of the object&apos;s List
+        /// </summary>
+        protected override DOList CreateDOList() => new DOLDriveAccount();
+    };
+}
---
+++ VkRadio.LowCode.TestBed/Generated/Gui/UiRegistry.cs
@@ -0,0 +1,49 @@
+﻿using VkRadio.LowCode.Gui.WinForms;
+using VkRadio.LowCode.TestBed.Generated.Gui.Launchers;
+
+namespace VkRadio.LowCode.TestBed.Generated.Gui
+{
+    /// <summary>
+    /// UI Launchers Registry
+    /// </summary>
+    public class UiRegistry
+    {
+        /// <summary>
+        /// Sole instance (Singleton)
+        /// </summary>
+        static UiRegistry _instance;
+        /// <summary>
+        /// Quick Select Storage
+        /// </summary>
+        QuickSelectStorage _quickSelectStorage = new QuickSelectStorage();
+        /// <summary>
+        /// UI Launcher for Drive Account objects
+        /// </summary>
+        UILauncher _uilDriveAccount;
+
+        /// <summary>
+        /// Private constructor of UI launchers
+        /// </summary>
+        UiRegistry()
+        {
+            _uilDriveAccount = new UilDriveAccount();
+        }
+        /// <summary>
+        /// Static constructor of UI launchers
+        /// </summary>
+        static UiRegistry() { _instance = new UiRegistry(); }
+
+        /// <summary>
+        /// Sole instance (Singleton)
+        /// </summary>
+        public static UiRegistry Instance { get { return _instance; } }
+        /// <summary>
+        /// Quick Select Storage
+        /// </summary>
+        public QuickSelectStorage QuickSelectStorage { get { return _quickSelectStorage; } }
+        /// <summary>
+        /// UI Launcher for Drive Account objects
+        /// </summary>
+        public UILauncher UilDriveAccount { get { return _uilDriveAccount; } set { _uilDriveAccount = value; } }
+    };
+}
