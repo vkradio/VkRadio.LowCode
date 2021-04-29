e473ec605859bdf88b900741e6763d1def14994c
vkradio <37143087+vkradio@users.noreply.github.com>
2021-04-29 14:21:28 +0500

    LOWC-2 TestBed: Added traditional WinForms Settings mechanism

--- VkRadio.LowCode.TestBed/MainForm.Designer.cs
+++ VkRadio.LowCode.TestBed/MainForm.Designer.cs
@@ -30,14 +30,17 @@ namespace VkRadio.LowCode.TestBed
         private void InitializeComponent()
         {
             this.ObjectListButton = new System.Windows.Forms.Button();
+            this.SqlServerTextBox = new System.Windows.Forms.TextBox();
+            this.DatabaseTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // ObjectListButton
             // 
-            this.ObjectListButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.ObjectListButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.ObjectListButton.Location = new System.Drawing.Point(80, 23);
+            this.ObjectListButton.Location = new System.Drawing.Point(80, 93);
             this.ObjectListButton.Name = "ObjectListButton";
             this.ObjectListButton.Size = new System.Drawing.Size(183, 23);
             this.ObjectListButton.TabIndex = 0;
@@ -45,11 +48,47 @@ namespace VkRadio.LowCode.TestBed
             this.ObjectListButton.UseVisualStyleBackColor = true;
             this.ObjectListButton.Click += new System.EventHandler(this.ObjectListButton_Click);
             // 
+            // SqlServerTextBox
+            // 
+            this.SqlServerTextBox.Location = new System.Drawing.Point(81, 17);
+            this.SqlServerTextBox.Name = "SqlServerTextBox";
+            this.SqlServerTextBox.Size = new System.Drawing.Size(153, 23);
+            this.SqlServerTextBox.TabIndex = 1;
+            // 
+            // DatabaseTextBox
+            // 
+            this.DatabaseTextBox.Location = new System.Drawing.Point(81, 46);
+            this.DatabaseTextBox.Name = "DatabaseTextBox";
+            this.DatabaseTextBox.Size = new System.Drawing.Size(153, 23);
+            this.DatabaseTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "SQL Server";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Database";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(333, 69);
+            this.ClientSize = new System.Drawing.Size(333, 139);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DatabaseTextBox);
+            this.Controls.Add(this.SqlServerTextBox);
             this.Controls.Add(this.ObjectListButton);
             this.MaximizeBox = false;
             this.Name = "MainForm";
@@ -58,12 +97,17 @@ namespace VkRadio.LowCode.TestBed
             this.Text = "vkradio Low Code Test Bed";
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button ObjectListButton;
+        private System.Windows.Forms.TextBox SqlServerTextBox;
+        private System.Windows.Forms.TextBox DatabaseTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- VkRadio.LowCode.TestBed/MainForm.cs
+++ VkRadio.LowCode.TestBed/MainForm.cs
@@ -1,48 +1,74 @@
 ﻿using System;
 using System.Data.SqlClient;
-using System.IO;
+using System.Diagnostics.CodeAnalysis;
 using System.Windows.Forms;
 using VkRadio.LowCode.Gui.WinForms;
 using VkRadio.LowCode.TestBed.Generated.Gui;
 using VkRadio.LowCode.TestBed.Generated.Model.Storage;
+using VkRadio.LowCode.TestBed.Properties;
 
 namespace VkRadio.LowCode.TestBed
 {
+    [SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "This app will not be launched on Windows version prior to 7")]
     public partial class MainForm : Form
     {
+        const string appName = "VkRadio.LowCode.TestBed";
+        bool loadSettings;
+
         public MainForm()
         {
             InitializeComponent();
+
+            SqlServerTextBox.TextChanged += SettingsChanged;
+            DatabaseTextBox.TextChanged += SettingsChanged;
         }
 
         void MainForm_Load(object sender, EventArgs e)
         {
-            // I just had no time to discover how to properly config WinForms apps in .NET 5,
-            // so I use a quick and dirty trick here. This is a plain text file with three rows:
-            // app name (any), server address (ex: "localhost"), database name (ex: "sometestdb").
-            var dbParams = File.ReadAllLines(@"C:\Users\vitaliy\Desktop\testbed.txt");
+            loadSettings = true;
+            SqlServerTextBox.Text = Settings.Default.SqlServer;
+            DatabaseTextBox.Text = Settings.Default.Database;
+            loadSettings = false;
 
-            var builder = new SqlConnectionStringBuilder
-            {
-                ApplicationName = dbParams[0],
-                DataSource = dbParams[1],
-                InitialCatalog = dbParams[2],
-                IntegratedSecurity = true
-            };
-
-            // Legacy way of using lots of Singletons everywhere. Now I'll almost never do that, I'll prefer
-            // DI instead (such as partially implemented in SqlClientFactory constructor here).
-            Orm.DbProviderFactory.Default = new Orm.SqlClientFactory(builder.ConnectionString);
-            StorageRegistry.Instance = new StorageRegistry(Orm.DbProviderFactory.Default);
-            if (UiRegistry.Instance == null)
-                throw new ApplicationException("UiRegistryExt.Instance == null");
-            QuickSelectStorage.ProgramFolder = dbParams[0];
+            QuickSelectStorage.ProgramFolder = appName;
         }
 
         void ObjectListButton_Click(object sender, EventArgs e)
         {
-            using var frm = UiRegistry.Instance.UilDriveAccount.CreateList();
-            frm.ShowDialog(this);
+            var builder = new SqlConnectionStringBuilder
+            {
+                ApplicationName = appName,
+                DataSource = Settings.Default.SqlServer,
+                InitialCatalog = Settings.Default.Database,
+                IntegratedSecurity = true
+            };
+
+            try
+            {
+                // Legacy way of using lots of Singletons everywhere. Now I'll almost never do that, I'll prefer
+                // DI instead (such as partially implemented in SqlClientFactory constructor here).
+                Orm.DbProviderFactory.Default = new Orm.SqlClientFactory(builder.ConnectionString);
+                StorageRegistry.Instance = new StorageRegistry(Orm.DbProviderFactory.Default);
+                if (UiRegistry.Instance == null)
+                    throw new ApplicationException("UiRegistryExt.Instance == null");
+
+                using var frm = UiRegistry.Instance.UilDriveAccount.CreateList();
+                frm.ShowDialog(this);
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show(this, "SQL Server or Database not properly set.");
+            }
+        }
+
+        void SettingsChanged(object sender, EventArgs e)
+        {
+            if (!loadSettings)
+            {
+                Settings.Default.SqlServer = SqlServerTextBox.Text.Trim();
+                Settings.Default.Database = DatabaseTextBox.Text.Trim();
+                Settings.Default.Save();
+            }
         }
     }
 }
